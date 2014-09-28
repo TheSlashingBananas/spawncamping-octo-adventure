d392bbc468542120f689a7af3b701b5aa2c57d67
XTSlasher <xt.slasher@gmail.com>
2014-09-28 01:19:35 -0500

    Beginnings of Something Great?

---
+++ eclipse/src/main/java/net/slasherxt/slashmod/SlashMod.java
@@ -0,0 +1,30 @@
+package net.slasherxt.slashmod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "SlashMod", name = "SlashMod", version = "1.7.2-1.0")
+public class SlashMod {
+	
+	@Instance("SlashMod")
+	public static SlashMod instance;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		
+	}
+}
